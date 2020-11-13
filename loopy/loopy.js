8769b41ed9c1aee0134e6f90dd1dee40588f0847
Masi <masii.shaneshin@gmail.com>
2020-11-13 01:11:15 -0500

    My loopy program

---
+++ loopy/loopy.js
@@ -0,0 +1,18 @@
+
+for (let i = 100 ; i < 200 ; i++) {
+  
+  if ( i % 3 === 0 ){
+    console.log("Loopy");
+    
+  } else if (  i % 4 === 0){
+   
+    console.log("Lighthouse");
+    
+  } else (i % 4 === 0 ) && ( i % 3 === 0){
+  
+    console.log("LoopyLighthouse");
+    
+  }
+  
+  
+}
